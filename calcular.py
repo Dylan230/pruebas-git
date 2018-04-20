07b3aa51982bef355b6b7083750ecb75ac2e6f27
Dylan230 <dylanalbertojossasi@gmail.com>
2018-04-20 19:52:13 -0300

    Permite calcular Suma y Resta

---
+++ calcular.py
@@ -0,0 +1,15 @@
+num1 = input("Ingrese un numero: ")
+
+num2 = input("Ingrese otro numero: ")
+
+calc = raw_input("Ingrese Sum o Res: ")
+
+if calc == "Sum":
+	res = num1 + num2
+elif calc == "Res":
+	res = num1 - num2
+else:
+	print "Ingrese una opcion valida"
+
+print "El resultado es: {}".format(res)
+
